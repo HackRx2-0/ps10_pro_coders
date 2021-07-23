91d3eba67eaf44fc9ee89dbc2b433f9d6092536f
Aaditya Singhal <aadityasinghal1978@gmail.com>
2021-07-23 18:33:58 +0530

    updated ui

--- app.py
+++ app.py
@@ -19,15 +19,20 @@ if data is not None:  # Here if block runs only when user gives dataset
     # Loading the dataset using pandas
     df = pd.read_csv(data)
 
+text("OR")
+
+real_Df = text_input("Enter Real Time data : ")
+
 lis = get_features()  # Getting the features from the dataset
 
-title("Let's create a table!")
+title("Let's Configure Weights!")
 
 k = 0
 for i in range(8):
     cols = beta_columns(4)
     for j in range(4):
         cols[j].number_input(f"Configure {lis[k]}",value=0.5,key=f"{lis[k]}",min_value=0.0,max_value=1.0,step=0.0001)
+        text("")
         k += 1
 
 if df is not None:
