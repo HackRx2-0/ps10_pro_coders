dece281f9ab812ca7af1ee893df3f3edd44c1a34
Abhay_Dhiman <abhaysdhimans@gmail.com>
2021-07-24 04:02:20 +0530

    Added match score functionality

--- modules/clusters.py
+++ modules/clusters.py
@@ -1,3 +1,24 @@
+import pandas as pd
+
+def get_clusters(id, df):
+    return df.loc[df['doctor_id'] == id]
+
+def match_score(row_1, row_2):
+    col_1 = row_1.values.tolist()[0]
+    col_2 = row_2.values.tolist()[0]
+    count_match = 0
+
+    mis_matches = 0
+    for column_1, column_2 in zip(col_1, col_2):
+        if column_1 and column_2:
+            if column_1 == column_2:
+                count_match += 1
+    
+    try:
+        return count_match / (len(col_1) - mis_matches)
+    except:
+        return 1
+
 def average(lst):
     return sum(lst) / len(lst)
 
